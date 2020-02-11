2476314d415a3781d420bef2f3d89c4d93761fd9
Thu Tran <thutranng@gmail.com>
2020-02-11 09:13:26 -0500

    simple product-addition match

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+# Product-Addition Match
+Simple usage of conditionals to implement rock-paper-scissors game rules: 
+
+1. Each player chooses an integer (input). 
+2. Player 1 will play the `player1_number * player2_number % 3`-th element in the choices array. Player 2 will play the `(player1_number + player2_number) % 3`-th element in the choices array.
+3. Use conditionals to decide the result of the match
+
+
 # Rock-Paper-Scissors Championship
 
 ## How the simulation works:
@@ -8,3 +16,4 @@
 ## Options for grouping people:
 1. `Simulate_Simple()`: the first (order of adding to the championship) player competes with the second, the winner of this first match will then play against the third player and so on. The last match will be between whoever has won previous match and the player who was last added to the championship. This is a very unfair way to decide who plays against whom.
 2. `Simulate_Quicksort_Like()`: recursively use pivot values to divide players into two teams then four and so on until pairs of players are finalized (similar to quicksort).
+
---
+++ product_addition_match.py
@@ -0,0 +1,32 @@
+def main():
+    choices = ["rock", "paper", "scissors"]
+    p1_number = input("Player 1 chooses an integer: ")
+    p2_number = input("Player 2 chooses an integer: ")
+
+    p1_choice = choices[abs(int(p1_number)) * abs(int(p2_number)) % 3]
+    p2_choice = choices[(abs(int(p2_number)) + abs(int(p1_number))) % 3]
+    print("Player 1 plays", p1_choice)
+    print("Player 2 plays", p2_choice)
+
+    p1_win_msg = "Player 1 wins"
+    p2_win_msg = "Player 2 wins"
+    tied_msg = "The match is tied"
+
+    if p1_choice=="rock" and p2_choice=="paper" :
+        return p2_win_msg
+    elif p2_choice=="rock" and p1_choice=="paper" :
+        return p1_win_msg
+    elif p1_choice=="paper" and p2_choice=="scissors" :
+        return p2_win_msg
+    elif p2_choice=="paper" and p1_choice=="scissors" :
+        return p1_win_msg
+    elif p1_choice=="scissors" and p2_choice=="rock" :
+        return p2_win_msg
+    elif p2_choice=="scissors" and p1_choice=="rock" :
+        return p1_win_msg
+    else :
+        return tied_msg
+
+if __name__ == "__main__":
+    result = main()
+    print(result)
